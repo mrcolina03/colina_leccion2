c4a83dd8e229f01b12ac83c43f1611879d42af8b
Mateo Ruben Colina Morales <mrcolina@espe.edu.ec>
2025-12-12 17:22:21 -0500

    Proyecto vacío

---
+++ src/main/java/espe/edu/ec/colinaleccion2/ColinaLeccion2Application.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.colinaleccion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColinaLeccion2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColinaLeccion2Application.class, args);
+    }
+
+}
---
+++ src/test/java/espe/edu/ec/colinaleccion2/ColinaLeccion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.colinaleccion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColinaLeccion2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
